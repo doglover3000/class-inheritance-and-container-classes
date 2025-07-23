0807e3d5eec852b09f1aa622e0345918b8a6f958
Эвелина <78720428+doglover3000@users.noreply.github.com>
2025-07-23 17:45:33 +0300

    Create Object.h

---
+++ Object.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <locale>
+using namespace std;
+class Object
+{
+public:
+Object();
+explicit Object(string& objName);
+~Object();
+void name(string& objName);
+string getName();
+private:
+string objName;
+};
