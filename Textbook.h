8aae4034d34e85c31a41e885a893830a63f166f7
Эвелина <78720428+doglover3000@users.noreply.github.com>
2025-07-23 17:48:02 +0300

    Create Textbook.h

---
+++ Textbook.h
@@ -0,0 +1,44 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <string>
+#include <locale>
+#include "Base.h"
+#include "Object.h"
+using namespace std;
+/*
+Для учебника определено: название, автор, год выпуска,
+для какого учебного заведения предназначено, год обучения, объем страниц,
+стоимость.
+*/
+class Textbook : public Base
+{
+public:
+Textbook();
+~Textbook();
+void setName(string& name);
+void setAuthor(string& auth);
+void setYear(string& year);
+void setSchool(string& sch);
+void setYearofStudy(string& styear);
+void setPageCount(string& pages);
+void setPrice(string& price);
+string getName();
+string getAuthor();
+string getSchool();
+string getYear();
+string getYearofStudy();
+string getPageCount();
+string getPrice();
+virtual int GetID() override;
+virtual void GetInfo() override;
+virtual void Edit() override;
+private:
+Object _object;
+string _author;
+string _year;
+string _school;
+string _study_year;
+string _page_cnt;
+string _price;
+};
