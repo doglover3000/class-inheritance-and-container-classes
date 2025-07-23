c8044ba4120a1630c7d6aa474f5509991f8a0c2a
Эвелина <78720428+doglover3000@users.noreply.github.com>
2025-07-23 17:45:50 +0300

    Create Object.cpp

---
+++ Object.cpp
@@ -0,0 +1,21 @@
+#include "Object.h"
+Object::Object()
+{
+objName = "object name";
+}
+Object::Object(string& objName)
+{
+this->name(objName);
+}
+Object::~Object()
+{
+objName.~basic_string();
+}
+void Object::name(string& name)
+{
+objName = name;
+}
+string Object::getName()
+{
+return objName;
+}
