c360bea15da25fcfceed0fc6ba2c6ee57abe3cb4
Эвелина <78720428+doglover3000@users.noreply.github.com>
2025-07-23 17:46:14 +0300

    Create Keeper.h

---
+++ Keeper.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Base.h"
+#include "ObjectofKeeper.h"
+class Keeper
+{
+public:
+Keeper();
+~Keeper();
+void add(Base& obj);
+void remove(int i);
+void remove_all();
+int getsize();
+Base& operator[](const int i);
+private:
+int _size;
+ObjectofKeeper* _first;
+};
