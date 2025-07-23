eec4cb1eed969bb0db50f773ce6d6ddc29bfd823
Эвелина <78720428+doglover3000@users.noreply.github.com>
2025-07-23 17:45:19 +0300

    Create base.h

---
+++ base.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <locale>
+using namespace std;
+class Base
+{
+public:
+virtual ~Base() {};
+virtual int GetID() = 0;
+virtual void Edit() = 0;
+virtual void GetInfo() = 0;
+};
