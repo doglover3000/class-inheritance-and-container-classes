b0cdbe7a9e65bd61d4e89430b3cd30083fd3622c
Эвелина <78720428+doglover3000@users.noreply.github.com>
2025-07-23 17:48:44 +0300

    Create Stationary.h

---
+++ Stationary.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Base.h"
+#include "Object.h"
+using namespace std;
+/*
+Для канцелярии определено: тип канцелярии, цвет, назначение,
+стоимость.
+*/
+class Stationary : public Base
+{
+public:
+Stationary();
+~Stationary();
+void setName(string& name);
+void setType(string& type);
+void setColour(string& color);
+void setPurpose(string& purp);
+void setPrice(string& price);
+string getName();
+string getType();
+string getColour();
+string getPurpose();
+string getPrice();
+virtual int GetID() override;
+virtual void GetInfo() override;
+virtual void Edit() override;
+private:
+Object _object;
+string _type;
+string _colour;
+string _purpose;
+string _price;
+};
